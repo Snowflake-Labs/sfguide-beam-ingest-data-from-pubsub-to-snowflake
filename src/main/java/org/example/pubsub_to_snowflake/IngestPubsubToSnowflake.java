7c9e851b8342988002c8c453d894ae349a2ca686
Kamil Bregula <kamil.bregula@snowflake.com>
2022-05-03 19:58:02 +0200

    fixup! Add source code

--- src/main/java/org/example/pubsub_to_snowflake/IngestPubsubToSnowflake.java
+++ src/main/java/org/example/pubsub_to_snowflake/IngestPubsubToSnowflake.java
@@ -40,48 +40,7 @@ import org.apache.beam.sdk.schemas.transforms.Convert;
 import org.apache.beam.sdk.transforms.JsonToRow;
 
 /**
- * An example that counts words in Shakespeare and includes Beam best practices.
- *
- * <p>This class, {@link IngestPubsubToSnowflake}, is the second in a series of four successively
- * more detailed 'word count' examples. You may first want to take a look at {@link
- * MinimalWordCount}. After you've looked at this example, then see the {@link DebuggingWordCount}
- * pipeline, for introduction of additional concepts.
- *
- * <p>For a detailed walkthrough of this example, see <a
- * href="https://beam.apache.org/get-started/wordcount-example/">
- * https://beam.apache.org/get-started/wordcount-example/ </a>
- *
- * <p>Basic concepts, also in the MinimalWordCount example: Reading text files; counting a
- * PCollection; writing to text files
- *
- * <p>New Concepts:
- *
- * <pre>
- *   1. Executing a Pipeline both locally and using the selected runner
- *   2. Using ParDo with static DoFns defined out-of-line
- *   3. Building a composite transform
- *   4. Defining your own pipeline options
- * </pre>
- *
- * <p>Concept #1: you can execute this pipeline either locally or using by selecting another runner.
- * These are now command-line options and not hard-coded as they were in the MinimalWordCount
- * example.
- *
- * <p>To change the runner, specify:
- *
- * <pre>{@code
- * --runner=YOUR_SELECTED_RUNNER
- * }</pre>
- *
- * <p>To execute this pipeline, specify a local output file (if using the {@code DirectRunner}) or
- * output prefix on a supported distributed file system.
- *
- * <pre>{@code
- * --output=[YOUR_LOCAL_FILE | YOUR_OUTPUT_PREFIX]
- * }</pre>
- *
- * <p>The input file defaults to a public data set containing the text of of King Lear, by William
- * Shakespeare. You can override it and choose your own input with {@code --inputFile}.
+ * An example that read a messages from Pubsub and writes to Snowflake.
  */
 public class IngestPubsubToSnowflake {
 
